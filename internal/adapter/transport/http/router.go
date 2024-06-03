ba9f4156106145dcfe9c37f203abf068d5a379ef
bulutcan99 <gocerbulutcan@gmail.com>
2024-06-03 15:42:55 +0300

    refactor: delete rate limiter

--- internal/adapter/transport/http/router.go
+++ internal/adapter/transport/http/router.go
@@ -1,23 +1,21 @@
 package http
 
-import "time"
-
 func (s *server) SetupRouter() {
 	s.hrManagementRouter()
 }
 
 func (s *server) hrManagementRouter() {
 	route := s.app.Group("/hr-management")
-	route.Post("/create-user", s.CreateUser, s.RateLimiter(5, time.Minute))
-	route.Post("/create-department", s.CreateDepartment, s.RateLimiter(5, time.Minute))
+	route.Post("/create-user", s.CreateUser)
+	route.Post("/create-department", s.CreateDepartment)
 }
 
 func (s *server) carManagementRouter() {
 	route := s.app.Group("/car-management")
-	route.Post("/create-car", s.CreateCar, s.RateLimiter(5, time.Minute))
-	route.Post("/create-brand", s.CreateBrand, s.RateLimiter(5, time.Minute))
-	route.Post("/create-model", s.CreateModel, s.RateLimiter(5, time.Minute))
-	route.Post("/create-color", s.CreateColor, s.RateLimiter(5, time.Minute))
-	route.Post("/create-fuel", s.CreateFuel, s.RateLimiter(5, time.Minute))
-	route.Post("/create-transmission", s.CreateTransmission, s.RateLimiter(5, time.Minute))
+	route.Post("/create-car", s.CreateCar)
+	route.Post("/create-brand", s.CreateBrand)
+	route.Post("/create-model", s.CreateModel)
+	route.Post("/create-color", s.CreateColor)
+	route.Post("/create-fuel", s.CreateFuel)
+	route.Post("/create-transmission", s.CreateTransmission)
 }
